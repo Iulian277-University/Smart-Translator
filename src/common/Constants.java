fd0f7d7c00bbf8d1dc4547dba10c15de04ebd955
Iulian277 <iulian27_marius@yahoo.com>
2021-11-24 19:49:51 +0200

    initial commit

---
+++ src/common/Constants.java
@@ -0,0 +1,9 @@
+package common;
+
+/** This is a handler class for holding paths of the input files */
+public final class Constants {
+    private Constants() {}
+
+    public static final String DICTIONARIES_DIRECTORY = "dictionaries";
+
+}
---
+++ src/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+/** This is class is the entry point of the program */
+public class Main {
+    public static void main(String[] args) {
+
+        // Process all input-dictionary files
+        System.out.println("Starting...");
+    }
+}
