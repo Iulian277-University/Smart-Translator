46d93cb8ab179f76b9ce8ba4f88cb5b28c561343
Iulian <iulian27_marius@yahoo.com>
2021-11-24 21:05:33 +0200

    added gson to maven dependencies

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,5 +1,3 @@
 
-7
-pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
 S
 #src/main/java/common/Constants.java,6\e\6ebd50c2038f1fef9135e3562ebde17286486774
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,11 +1,16 @@
 package main;
 
+import com.google.gson.Gson;
+
 /** This is class is the entry point of the program */
 public class Main {
     public static void main(String[] args) {
 
         // Process all input-dictionary files
         System.out.println("Starting...");
+
+        // Deserialize json into class models
+
 
     }
 }
