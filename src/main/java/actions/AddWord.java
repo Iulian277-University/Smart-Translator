9eb2005bb448e11490589bd48182c55b653883e8
Iulian <iulian27_marius@yahoo.com>
2021-11-25 21:27:54 +0200

    TODO: work on actions

---
+++ src/main/java/actions/AddWord.java
@@ -0,0 +1,11 @@
+package actions;
+
+import entities.Word;
+
+public final class AddWord {
+    private AddWord() {}
+
+    public static boolean addWord(Word word, String language) {
+        return true;
+    }
+}
